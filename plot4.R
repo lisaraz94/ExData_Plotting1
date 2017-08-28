84d4777b0ff0c2a12fd040ec012d4521576a4147
lisaraz94 <razouml1@gmail.com>
2017-08-27 21:09:50 -0400

    updated legend

--- plot4.R
+++ plot4.R
@@ -28,7 +28,7 @@ plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
 plot(datetime, sub_metering1, type="l", ylab="Energy sub metering", xlab="")
 lines(datetime, sub_metering2, type="l", col="red")
 lines(datetime, sub_metering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=3, col=c("black", "red", "blue"), bty="n")
 
 plot(datetime, global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 
